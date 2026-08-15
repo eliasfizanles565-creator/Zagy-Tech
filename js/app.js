783e0998f9a28ccec0f8af650f9601412ce916d3
unknown <eliasfizanles565@gmail.com>
2026-08-15 14:53:08 -0500

    Subida automatica inteligente

--- js/app.js
+++ js/app.js
@@ -267,7 +267,7 @@ const heroSwiper = new Swiper('.hero-swiper', {
     speed: 500,
 
     // Separación entre Cards
-    spaceBetween: 10,
+    spaceBetween: 6,
     
     // Autoplay cada 5 segundos
     autoplay: {
